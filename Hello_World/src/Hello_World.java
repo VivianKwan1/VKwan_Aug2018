509f459796ac9ca0bb312a74ac990ccc93256ea1
VivianKwan1 <21kwanv58@stu.smuhsd.org>
2018-08-20 09:45:42 -0700

    first project to commit to github

---
+++ Hello_World/src/Hello_World.java
@@ -0,0 +1,14 @@
+
+public class Hello_World {
+	/**
+	 * Print out a message saying Hello World
+	 * 
+	 * @author Vivian Kwan
+	 * @version 8/17/2018
+	 */
+	
+	    public static void main(String[] args)
+	    {
+	        System.out.println("Hello World! From Vivian"); //Display the string
+	    }
+}
